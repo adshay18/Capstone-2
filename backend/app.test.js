8493b3399080b56cbf24f0a5f9ace15779e066b5
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-06-03 17:29:47 -0400

    test app

--- backend/app.test.js
+++ backend/app.test.js
@@ -4,13 +4,13 @@ const app = require('./app');
 const db = require('./db');
 
 test('not found for site 404', async function() {
-	const resp = await request(app).get('/no-such-path');
+	const resp = await request(app).get('/doesnotexist');
 	expect(resp.statusCode).toEqual(404);
 });
 
 test('not found for site 404 (test stack print)', async function() {
 	process.env.NODE_ENV = '';
-	const resp = await request(app).get('/no-such-path');
+	const resp = await request(app).get('/doesnotexist');
 	expect(resp.statusCode).toEqual(404);
 	delete process.env.NODE_ENV;
 });
