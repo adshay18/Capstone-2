3dbdbe4f8268185965205eb4e654091a3da4ff93
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-06-06 16:26:56 -0400

    finish user model, add partial sql function for updates

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const db = require('../db');
 const bcrypt = require('bcrypt');
 const { NotFoundError, BadRequestError } = require('../expressError');
 const { BCRYPT_WORK_FACTOR } = require('../config');
+const { updateSql } = require('./helpers');
 
 class User {
 	// User authentication
@@ -66,4 +67,77 @@ class User {
 
 		return user;
 	}
+
+	// Get a user
+	// returns {username, firstName, lastName, email, age, completedTasks, avatar}
+
+	static async get(username) {
+		const userRes = await db.query(
+			`SELECT username,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email,
+				  age,
+				  completed_tasks AS "completedTasks",
+				  avatar
+           FROM users
+           WHERE username = $1`,
+			[ username ]
+		);
+
+		const user = userRes.rows[0];
+		if (!user) throw new NotFoundError(`No user: ${username}`);
+		return user;
+	}
+
+	// Update user with data
+	// Does not need to be a complete update
+	// Data can include {firstName, lastName, password, email, age, avatar, completedTasks}
+	// returns {username, firstName, lastName, email, age, avatar, completedTasks}
+	// must be verified to run this function as passwords can be changed
+
+	static async update(username, data) {
+		if (data.password) {
+			data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+		}
+
+		const { setCols, values } = updateSql(data, {
+			firstName: 'first_name',
+			lastName: 'last_name',
+			completedTasks: 'completed_tasks'
+		});
+		const usernameIdx = '$' + (values.length + 1);
+
+		const query = `	UPDATE users
+						SET ${setCols}
+						WHERE username = ${usernameIdx}
+						RETURNING username, first_name AS "firstName", last_name AS "last_name", email, age, avatar, completed_tasks AS "completedTasks"`;
+
+		const result = await db.query(query, [ ...values, username ]);
+		const user = result.rows[0];
+
+		if (!user) throw new NotFoundError(`No user found named ${username}`);
+
+		delete user.password;
+		return user;
+	}
+
+	// Remove a user from db
+	// Returns {deleted: username}
+	static async remove(username) {
+		let result = await db.query(
+			`DELETE
+           FROM users
+           WHERE username = $1
+           RETURNING username`,
+			[ username ]
+		);
+		const user = result.rows[0];
+
+		if (!user) throw new NotFoundError(`No user found named ${username}`);
+
+		return { deleted: username };
+	}
 }
+
+module.exports = User;
