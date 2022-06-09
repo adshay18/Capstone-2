bc5c9b1efd6bce7bd09e0e7d7b17d1fefa956ccb
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-06-09 17:41:53 -0400

    write methods for Task model

--- backend/models/task.js
+++ backend/models/task.js
@@ -4,6 +4,8 @@ const db = require('../db');
 const { NotFoundError, BadRequestError } = require('../expressError');
 
 class Task {
+	// Add a new task associated to a user
+	// Returns {task_id, username, description, completed}
 	static async add(username, description) {
 		const duplicateCheck = await db.query(
 			`SELECT * FROM tasks
@@ -15,5 +17,53 @@ class Task {
 		if (duplicateCheck.rows[0]) {
 			throw new BadRequestError(`User already has task in their list`);
 		}
+
+		const result = await db.query(
+			`
+        INSERT INTO tasks
+        (username, description)
+        VALUES ($1, $2)
+        RETURNING *`,
+			[ username, description ]
+		);
+
+		const task = result.rows[0];
+		return task;
+	}
+
+	// Mark a task as completed
+	// Returns {task_id, username, description, completed}
+	static async markComplete(taskId) {
+		const result = await db.query(
+			`
+        UPDATE tasks
+        SET completed = true
+        WHERE task_id = $1
+        RETURNING *`,
+			[ taskId ]
+		);
+
+		const task = result.rows[0];
+
+		if (!task) throw new NotFoundError(`Task not found with ID: ${taskId}`);
+		return task;
+	}
+
+	// Remove a task from user's list
+	// Returns {deleted: taskId}
+
+	static async remove(taskId) {
+		const result = await db.query(
+			`
+        DELETE FROM tasks
+        WHERE task_id = $1`,
+			[ taskId ]
+		);
+
+		const deletedTask = result.rows[0];
+		if (!deletedTask) throw new NotFoundError(`Task not found with ID: ${taskId}`);
+		return { delted: taskId };
 	}
 }
+
+module.exports = Task;
