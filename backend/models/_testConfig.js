30bda6023317f10c1eb80aefe557240b3c4ae4c1
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-06-10 16:06:40 -0400

    update testing config to prepare for collectedBadges testing

--- backend/models/_testConfig.js
+++ backend/models/_testConfig.js
@@ -13,13 +13,14 @@ async function beforeAllTests() {
 	// Empty test database
 	await db.query('DELETE FROM users');
 	await db.query('DELETE FROM tasks');
+	await db.query('DELETE FROM collected_badges');
 	// Fill with user data
 	await db.query(
 		`
-        INSERT INTO users(username, password, first_name, last_name, email, age)
-        VALUES  ('Test1', $1, 'test', 'one', 'test.1@gmail.com', 19),
-                ('Test2', $2, 'test', 'two', 'test.2@gmail.com', 42),
-                ('Test3', $3, 'test', 'three', 'test.3@gmail.com', 15)`,
+        INSERT INTO users(username, password, first_name, last_name, email, age, completed_tasks)
+        VALUES  ('Test1', $1, 'test', 'one', 'test.1@gmail.com', 19, 10),
+                ('Test2', $2, 'test', 'two', 'test.2@gmail.com', 42, 1),
+                ('Test3', $3, 'test', 'three', 'test.3@gmail.com', 15, 504)`,
 		[
 			await bcrypt.hash('testpass1', BCRYPT_WORK_FACTOR),
 			await bcrypt.hash('testpass2', BCRYPT_WORK_FACTOR),
@@ -45,6 +46,24 @@ async function beforeAllTests() {
 			taskDescriptions.cooking,
 			taskDescriptions.cooking
 		]
+	);
+
+	// Give users badges for number of complete tasks
+	await db.query(
+		`INSERT INTO collected_badges (badge_id, username)
+         VALUES (1, 'Test1'),
+                (2, 'Test1'),
+                (3, 'Test1'),
+                (1, 'Test2'),
+                (1, 'Test3'),
+                (2, 'Test3'),
+                (3, 'Test3'),
+                (4, 'Test3'),
+                (5, 'Test3'),
+                (6, 'Test3'),
+                (7, 'Test3'),
+                (8, 'Test3'),
+                (9, 'Test3')`
 	);
 }
 
