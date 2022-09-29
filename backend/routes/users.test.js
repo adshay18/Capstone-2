24be6dd392adc51c7b83b8c9c12038f8646c57d7
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-09-29 07:15:06 -0400

    continued user route testing

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -27,4 +27,65 @@ describe('POST /users/register', function() {
 		});
 		expect(res.statusCode).toEqual(201);
 	});
+	test('bad request missing data', async function() {
+		const res = await request(app).post('/users/register').send({
+			firstName: 'Bad',
+			lastName: 'Data'
+		});
+		expect(res.statusCode).toEqual(400);
+	});
+});
+
+/************** POST /users/token */
+
+describe('POST /users/token', function() {
+	test('creates new token', async function() {
+		const res = await request(app).post('/users/token').send({
+			username: 'u1',
+			password: 'password1'
+		});
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.token).toEqual(expect.any(String));
+	});
+
+	test('needs correct password', async function() {
+		const res = await request(app).post('/users/token').send({
+			username: 'u1',
+			password: 'misinput'
+		});
+		expect(res.statusCode).toEqual(400);
+	});
+
+	test('404 user not found error', async function() {
+		const res = await request(app).post('/users/token').send({
+			username: 'notarealuser',
+			password: 'wrong'
+		});
+		expect(res.statusCode).toEqual(404);
+	});
+});
+
+/************** GET /users/username */
+
+describe('GET /users/:username', function() {
+	test('finds user', async function() {
+		const res = await request(app).get('/users/u1');
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toEqual({
+			user: {
+				username: 'u1',
+				firstName: 'U1F',
+				lastName: 'U1L',
+				email: 'user1@user.com',
+				age: 15,
+				completedTasks: 0,
+				avatar: null
+			}
+		});
+	});
+
+	test('404 user not found', async function() {
+		const res = await request(app).get('/users/notausername');
+		expect(res.statusCode).toEqual(404);
+	});
 });
