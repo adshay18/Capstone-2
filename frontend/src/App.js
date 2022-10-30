5dc736451b84ac0234a20ae2148cfb24b97f5651
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-10-30 15:55:22 -0400

    restore git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,13 +43,9 @@ function App() {
 		<UserContext.Provider value={{ currUser, token, login, logout, signup }}>
 			<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					Kelly is cute.
 				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
 			</header>
 		</div>
 		</UserContext.Provider>
