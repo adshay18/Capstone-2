1a043440c24f00c12888d34732883baef4f2b200
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-11-06 11:41:03 -0500

    create functional user component

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,25 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Card, CardBody, CardTitle } from 'reactstrap';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import BoredApi from './Api';
 import UserContext from './UserContext';
 import './Home.css';
 
 const User = () =>{
-    // const {currUser} = useContext(UserContext)
-    let {username} = useParams()
-    console.log(username)
-    
+    const {currUser} = useContext(UserContext);
+    let {username} = useParams();
+    const [userDetails, setUserDetails] = useState(null);
+    const [notFound, setNotFound] = useState(false)
+    const [badges, setBadges] = useState(null)
+
+    useEffect(()=>{
+        BoredApi.getUser(username)
+            .then(res => setUserDetails(res.user))
+            .then(res => setNotFound(false))
+            .catch(err => setNotFound(true))
+        if(!notFound) {
+            BoredApi.getBadges(username).then(res => setBadges(res.badges)).catch(err => setBadges([]))
+        }
+    }, [username])
+
     return (
+        
         <section className='col-md-8'>
             <Card>
-                <CardBody className='text-center'>
-                <CardTitle className="font-weight-bold">
-						<b>{username}</b>
+                {notFound ? '404 not found' : <CardBody className='text-center'>
+                    <CardTitle className="font-weight-bold">
+						<b>{userDetails ? userDetails.username : "Loading..."}</b>
 					</CardTitle>
-                </CardBody>
+                </CardBody>}
             </Card>
         </section>
-    )
+    );
 };
 
 export default User;
