60f8559796eaa6a1431a2f97e765c801c4eae6a5
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-11-15 15:57:31 -0500

    make navbar responsive

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -13,27 +13,38 @@ function NavBar() {
 				<NavLink exact to="/" className="navbar-brand">
 					Not Bored
 				</NavLink>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    				<span className="navbar-toggler-icon"></span>
+  				</button>
 
-				{currUser.username ? (
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink to="/do-something">Do Something</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink to={`/users/${currUser.username}`}>@{currUser.username}</NavLink>
-						</NavItem>
-						<Link exact to="/"><Button onClick={logout} className="shadow">logout</Button></Link>
-					</Nav>
-				) : (
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink to="/login">Login</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink to="/signup">Signup</NavLink>
-						</NavItem>
-					</Nav>
-				)}
+				  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+					{currUser.username ? (
+						<Nav navbar>
+							<NavItem>
+								<NavLink to="/do-something">Do Something</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink to={`/users/${currUser.username}`}>@{currUser.username}</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink exact to="/">
+									<Button onClick={logout} className="shadow">logout</Button>
+								</NavLink>
+							</NavItem>
+							
+						</Nav>
+					) : (
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink to="/login">Login</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink to="/signup">Signup</NavLink>
+							</NavItem>
+						</Nav>
+					)}
+				  </div>
+				
 			</Navbar>
 		</div>
 	);
