41850f0c0fd4f29e0ed82ca26afbacda9e5217e7
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-11-16 08:49:11 -0500

    style activity form

--- frontend/src/ActivityForm.js
+++ frontend/src/ActivityForm.js
@@ -61,59 +61,65 @@ const ActivityForm = () => {
     }
 
 	return (
-        <div>
-            <Card>
-			<CardTitle>Bored No More!</CardTitle>
-			{errors ? <CardSubtitle>{errors}</CardSubtitle> : null}
-			<CardBody>
-				<Form onSubmit={handleSubmit}>
-					<FormGroup>
-						<Label htmlFor="type">Type</Label>
-						<select
-							id="type"
-							name="type"
-                            value={formData.type}
-							onChange={handleChange}
-							required
-						>
-                            {types.map(type => <option value={type} key={Math.random()}>{type}</option>)}
-                        </select>
-					</FormGroup>
-					<FormGroup>
-						<Label htmlFor="participants">Participants</Label>
-                        <select
-							id="participants"
-							name="participants"
-                            value={formData.participants}
-							onChange={handleChange}
-							required
-						>
-                            {participants.map(num => <option value={num} key={Math.random()}>{num}</option>)}
-                        </select>
-					</FormGroup>
-					<FormGroup>
-						<Label htmlFor="price">Free</Label>
-						<select
-                            id="price"
-                            name="price"
-                            vale={formData.price}
-                            onChange={handleChange}
-                            required>
-                            <option value="yes" key={1}>Yes</option>
-                            <option value="no" key={2}>No</option>
-                        </select>
-					</FormGroup>
-					<Button>Submit</Button>
-				</Form>
-                <Button onClick={handleClick}>Random</Button>
-			</CardBody>
-		</Card>
-        {submitted ? 
-            <div>
-               {results.error ? <p>"Oops! Try different filters or a random activity"</p> : <ActivityCard id={+results.key} key={results.key}/>}
+        <section className='col-md-12 justify-content-center shadow-none'>
+            <div className="container mb-4 pt-5 d-flex justify-content-center">
+                <Card className='border-0 shadow'>
+                    <CardTitle>Bored No More!</CardTitle>
+                    {errors ? <CardSubtitle>{errors}</CardSubtitle> : null}
+                    <CardBody>
+                        <Form onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label htmlFor="type">Type</Label>
+                                <select
+                                    id="type"
+                                    name="type"
+                                    value={formData.type}
+                                    onChange={handleChange}
+                                    required
+                                    className='form-select shadow'
+                                >
+                                    {types.map(type => <option value={type} key={Math.random()}>{type}</option>)}
+                                </select>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="participants">Participants</Label>
+                                <select
+                                    id="participants"
+                                    name="participants"
+                                    value={formData.participants}
+                                    onChange={handleChange}
+                                    required
+                                    className='form-select shadow'
+                                >
+                                    {participants.map(num => <option value={num} key={Math.random()}>{num}</option>)}
+                                </select>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="price">Free</Label>
+                                <select
+                                    id="price"
+                                    name="price"
+                                    vale={formData.price}
+                                    onChange={handleChange}
+                                    required className='form-select shadow'>
+                                    <option value="yes" key={1}>Yes</option>
+                                    <option value="no" key={2}>No</option>
+                                </select>
+                            </FormGroup>
+                            <Button className='bg-success shadow'>Submit</Button>
+                        </Form>
+                        <br></br>
+                        <Button className="shadow" onClick={handleClick}>Random</Button>
+                    </CardBody>
+                </Card>
+                
             </div>
-            : null}
-        </div>
+            {submitted ? 
+                    <div className='border-0 rounded results container mb-4 pt-5 d-flex justify-content-center'>
+                    {results.error ? <p>"Oops! Try different filters or a random activity"</p> : <ActivityCard id={+results.key} key={results.key}/>}
+                    </div>
+                : null}
+        </section>
 		
 	);
 };
